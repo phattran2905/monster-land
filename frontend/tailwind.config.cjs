09a2f3b0440b341497228989de8ba3d09395b37a
phat@minim1 <phat.tranducvn@gmail.com>
2023-02-10 15:03:28 -0800

    add tailwind configuration file

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+        colors: {
+            'primary': '#393E7D',
+            'secondary': '#E5446D',
+            'diamond': '#D7B1FD',
+            'coins': '#ECD07C',
+            'stamina': '#D2FF89',
+            'success': '#2C8837',
+            'fail': '#DB0039',
+            'level': '#52796F'
+        },
 		extend: {},
 	},
 	plugins: [],
