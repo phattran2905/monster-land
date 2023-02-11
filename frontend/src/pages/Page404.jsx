d98af2d3b9896e8b5b03aa7a721232b811ef67d7
phat@minim1 <phat.tranducvn@gmail.com>
2023-02-11 14:08:11 -0800

    Add back to homepage button

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -3,21 +3,29 @@ import Footer from "../components/Footer"
 import code404Img from "../assets/img/404.png"
 
 export default function Page404() {
-  return (
-    <div className="container-xl flex flex-row h-screen">
-        <div className="basis-2/5 bg-Indigo-Blue flex flex-col items-stretch justify-between">
-            <div className="w-full h-full flex flex-col justify-center items-center">
-                <img
-                    src={code404Img}
-                    alt="Code 404"
-                />
-                <p className="text-white font-bold text-center text-4xl">Page not found</p>
-            </div>
+	return (
+		<div className="container-xl flex flex-row h-screen">
+			<div className="basis-2/5 bg-Indigo-Blue flex flex-col items-stretch justify-between">
+				<div className="w-full h-full flex flex-col justify-center items-center">
+					<img
+						src={code404Img}
+						alt="Code 404"
+					/>
+					<p className="mb-4 text-white font-bold text-center text-6xl">Page not found</p>
 
-            <Footer />
-        </div>
-        <div className="basis-3/5 bg-Flamingo-Pink bg-background-img-3 bg-no-repeat bg-cover">
-        </div>
-    </div>
-  )
+					<div className="m-12">
+						<Link
+							to="/home"
+							className="h-full w-full py-5 px-10 rounded-full bg-Flamingo-Pink hover:bg-white  text-white text-center font-bold text-xl hover:text-Indigo-Blue transition-colors duration-500"
+						>
+							Back to Homepage
+						</Link>
+					</div>
+				</div>
+
+				<Footer />
+			</div>
+			<div className="basis-3/5 bg-Flamingo-Pink bg-background-img-3 bg-no-repeat bg-cover"></div>
+		</div>
+	)
 }
