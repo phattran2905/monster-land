3b5c2e6da0a5ae517f68bb5731394f08bd768c42
phat@minim1 <phat.tranducvn@gmail.com>
2023-02-11 12:36:02 -0800

    Add capacity

--- frontend/src/pages/PokemonCollectionPage.jsx
+++ frontend/src/pages/PokemonCollectionPage.jsx
@@ -8,10 +8,14 @@ export default function PokemonCollectionPage() {
 		<div className="container-xl flex flex-col h-screen justify-between">
 			<Header />
 
-			<div className="w-full h-full flex flex-row">
+			<div className="w-full h-full flex flex-row relative">
 				<MenuBar />
 
-				<div className="m-16 w-full">
+				<div className="bg-Flamingo-Pink rounded-md py-2 px-3 absolute right-5 top-5">
+					<span className="text-white font-bold text-sm">8/50</span>
+				</div>
+
+				<div className="m-12 w-full">
 					<div className="flex flex-row flex-wrap gap-y-10 gap-x-28 mx-auto">
 						<PokemonCard />
 						<PokemonCard />
