f33a9c485b52fc9cfea71a0a456fa5c0ec62d9c5
phat@minim1 <phat.tranducvn@gmail.com>
2023-02-12 15:35:50 -0800

    Add Pokemon Controller

---
+++ backend/src/controllers/PokemonController.js
@@ -0,0 +1,55 @@
+import PokemonModel from "../models/PokemonModel.js"
+
+// Populate Pokemon data for frontend to render
+const populatePokemonData = (pokemonDoc) => ({
+	uid: pokemonDoc.uid,
+	level: pokemonDoc.level,
+	exp: pokemonDoc.exp,
+	level_up_exp: pokemonDoc.level_up_exp,
+	power: pokemonDoc.power,
+	capture_rate: pokemonDoc.capture_rate,
+	name: pokemonDoc.info.name,
+	img_name: pokemonDoc.info.img_name,
+	level_up_exp_rate: pokemonDoc.info.level_up_exp_rate,
+	type: pokemonDoc.info.pkmType.map((i) => i.name),
+	status: pokemonDoc.status,
+})
+
+// Get A Pokemon by id
+export const getPokemonById = async (req, res) => {
+	try {
+		const pokemonDoc = await PokemonModel.findOne({ uid: req.params.id }).populate({
+			path: "info",
+			populate: { path: "pkmType", select: "-_id -uid name" },
+		})
+
+		if (!pokemonDoc) {
+			return res.status(404).json({ message: "Not Found" })
+		}
+
+		const pokemon = populatePokemonData(pokemonDoc)
+
+		return res.status(200).json(pokemon)
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ message: error.message })
+	}
+}
+
+// Get All Pokemon
+export const getAllPokemon = async (req, res) => {
+	try {
+		const criteria = req.query.status ? { status: req.query.status } : null
+
+		const pokemonListDoc = await PokemonModel.find(criteria).populate({
+			path: "info",
+			populate: { path: "pkmType", select: "-_id -uid name" },
+		})
+
+		const pokemonList = pokemonListDoc.map((pokemon) => populatePokemonData(pokemon))
+
+		return res.status(200).json(pokemonList)
+	} catch (error) {
+		return res.status(500).json({ message: error.message })
+	}
+}
--- backend/src/routes/Pokemon.js
+++ backend/src/routes/Pokemon.js
@@ -1,53 +1,10 @@
 import { Router } from "express"
-import { v4 as uuidv4 } from "uuid"
-import PokemonInfoModel from "../models/PokemonInfoModel.js"
-import PokemonModel from "../models/PokemonModel.js"
-import PokemonTypeModel from "../models/PokemonTypeModel.js"
+import { getPokemonById, getAllPokemon } from "../controllers/PokemonController.js"
 
 const router = Router()
 
 router
-	.get("/pokemon", (req, res) => {
-		res.json("Pokemon routes")
-	})
-	.post("/pokemon", async (req, res) => {
-		try {
-			const randomId = uuidv4()
-			const electricType = await PokemonTypeModel.create({
-				uid: randomId,
-				name: "Electric",
-			})
-
-			res.json(electricType)
-		} catch (error) {
-			console.error(error)
-		}
-	})
-	.get("/pokemon/:id", async (req, res) => {
-		try {
-			const pokemonDoc = await PokemonModel.findOne({ uid: req.params.id }).populate({
-				path: "info",
-				populate: { path: "pkmType", select: "-_id -uid name" },
-			})
-
-			const pokemon = {
-				uid: pokemonDoc.uid,
-				level: pokemonDoc.level,
-				exp: pokemonDoc.exp,
-				level_up_exp: pokemonDoc.level_up_exp,
-				power: pokemonDoc.power,
-				capture_rate: pokemonDoc.capture_rate,
-				name: pokemonDoc.info.name,
-				img_name: pokemonDoc.info.img_name,
-				level_up_exp_rate: pokemonDoc.info.level_up_exp_rate,
-				type: pokemonDoc.info.pkmType.map((i) => i.name),
-			}
-
-			res.status(200).json(pokemon)
-		} catch (error) {
-			console.error(error)
-			res.status(500).json({ message: error.message })
-		}
-	})
+	.get("/pokemon", getAllPokemon)
+	.get("/pokemon/:id", getPokemonById)
 
 export default router
