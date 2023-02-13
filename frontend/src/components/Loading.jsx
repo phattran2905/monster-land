a4e65aafc6b7fba6892e053b4048c60662f95aeb
phat@minim1 <phat.tranducvn@gmail.com>
2023-02-13 15:45:40 -0800

    Add loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+// Source code: https://loading.io/css/
+export default function Loading() {
+	return (
+		<div className="lds-ripple inline-block relative w-20 h-20">
+			<div className="absolute opacity-100 rounded-full border-4 border-Flamingo-Pink animate-ripple-1"></div>
+			<div className="absolute opacity-100 rounded-full border-4 border-Flamingo-Pink  animate-ripple-2"></div>
+		</div>
+	)
+}
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -24,6 +24,44 @@ module.exports = {
 				"background-img-4": "url('/img/bg/bg-4.png')",
 				"background-img-5": "url('/img/bg/bg-5.png')",
 			},
+			keyframes: {
+				// Source code: https://loading.io/css/
+				"lds-ripple": {
+					"0%": {
+						top: "36px",
+						left: "36px",
+						width: 0,
+						height: 0,
+						opacity: 0,
+					},
+					"4.9%": {
+						top: "36px",
+						left: "36px",
+						width: 0,
+						height: 0,
+						opacity: 0,
+					},
+					"5%": {
+						top: "36px",
+						left: "36px",
+						width: 0,
+						height: 0,
+						opacity: 1,
+					},
+					"100%": {
+						top: 0,
+						left: 0,
+						width: "72px",
+						height: "72px",
+						opacity: 0,
+					},
+				},
+			},
+
+			animation: {
+				"ripple-1": "lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite",
+				"ripple-2": "lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite -0.5s",
+			},
 		},
 	},
 	plugins: [],
