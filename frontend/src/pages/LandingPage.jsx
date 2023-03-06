813afd5e949fc9ff63946aeb7348d038af6e8ab2
phat@minim1 <phat.tranducvn@gmail.com>
2023-03-06 09:42:44 -0800

    Adjust logo and play button

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -6,27 +6,28 @@ export default function LandingPage() {
 	return (
 		<div className="container-xl flex flex-row h-screen">
 			<div className="basis-2/5 bg-Royal-Blue flex flex-col items-stretch justify-between">
-				<div className="w-3/4 mt-32 mx-auto">
-					<img
-						className="rounded-full"
-						src={logo}
-						alt="Pokemon Land logo"
-					/>
-				</div>
+				<div>
+					<div className="w-3/4 mt-32 mb-20 mx-auto">
+						<img
+							className="rounded-full"
+							src={logo}
+							alt="Monster Land logo"
+						/>
+					</div>
 
-				<div className="flex flex-row justify-center">
-					<Link
-						to="/home"
-						className="hover:shadow-2xl hover:shadow-Flamingo-Pink bg-Flamingo-Pink hover:bg-white rounded-full px-32 py-6 text-white hover:text-Flamingo-Pink font-bold text-4xl uppercase duration-1000 transition-colors"
-					>
-						Play
-					</Link>
+					<div className="flex flex-row justify-center">
+						<Link
+							to="/home"
+							className="border-4 border-y-Flamingo-Pink bg-Flamingo-Pink hover:bg-white rounded-full px-32 py-6 text-white hover:text-Flamingo-Pink font-bold text-4xl uppercase hover:underline duration-1000 transition-colors"
+						>
+							Play
+						</Link>
+					</div>
 				</div>
 
 				<Footer />
 			</div>
-			<div className="basis-3/5 bg-Flamingo-Pink bg-background-img-3 bg-no-repeat bg-cover">
-			</div>
+			<div className="basis-3/5 bg-Flamingo-Pink bg-background-img-3 bg-no-repeat bg-cover"></div>
 		</div>
 	)
 }
