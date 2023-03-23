212a6d7916213f4c78de1f1e75a3801a15882e4f
ducphat@Mbp2019 <phat.tranducvn@gmail.com>
2023-03-22 23:25:28 -0700

    Add middlewares to check JWT token

--- backend/src/routes/Backpack.js
+++ backend/src/routes/Backpack.js
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import { getItemsFromBackpack, useItemsOnMonster } from "../controllers/BackpackController.js"
+import validateJwt from "../middleware/JwtTokenHandler.js"
 
 const router = Router()
 
-router.get("/backpack", getItemsFromBackpack).put("/backpack/use", useItemsOnMonster)
+router
+	.get("/backpack", validateJwt, getItemsFromBackpack)
+	.put("/backpack/use", validateJwt, useItemsOnMonster)
 
 export default router
--- backend/src/routes/Trainer.js
+++ backend/src/routes/Trainer.js
@@ -4,12 +4,13 @@ import {
 	getTrainerInfo,
 	updateTrainerInfo,
 } from "../controllers/TrainerController.js"
+import validateJwt from "../middleware/JwtTokenHandler.js"
 
 const router = Router()
 
 router
-	.post("/trainer", createCharacter)
-	.get("/trainer", getTrainerInfo)
-	.put("/trainer", updateTrainerInfo)
+	.post("/trainer", validateJwt, createCharacter)
+	.get("/trainer", validateJwt, getTrainerInfo)
+	.put("/trainer", validateJwt, updateTrainerInfo)
 
 export default router
