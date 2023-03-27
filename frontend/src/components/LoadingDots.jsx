aa07abfd0f6939bd59a15a2299449c91493ecbf3
phat@minim1 <phat.tranducvn@gmail.com>
2023-03-26 17:32:00 -0700

    Change loading dots color

--- frontend/src/components/LoadingDots.jsx
+++ frontend/src/components/LoadingDots.jsx
@@ -3,9 +3,9 @@ function LoadingDots() {
 	return (
 		<div class="lds-ellipsis inline-block relative w-5 h-5">
 			<div className="absolute w-3 h-3 top-8 rounded-full bg-Flamingo-Pink ease-[cubic-bezier(0,1,1,0)] animate-ellipsis-1 left-2"></div>
-			<div className="absolute w-3 h-3 top-8 rounded-full bg-Dim-Gray ease-[cubic-bezier(0,1,1,0)] animate-ellipsis-2 left-2"></div>
+			<div className="absolute w-3 h-3 top-8 rounded-full bg-Flamingo-Pink ease-[cubic-bezier(0,1,1,0)] animate-ellipsis-2 left-2"></div>
 			<div className="absolute w-3 h-3 top-8 rounded-full bg-Flamingo-Pink ease-[cubic-bezier(0,1,1,0)] animate-ellipsis-3 left-8"></div>
-			<div className="absolute w-3 h-3 top-8 rounded-full bg-Dim-Gray ease-[cubic-bezier(0,1,1,0)] animate-ellipsis-4 left-14"></div>
+			<div className="absolute w-3 h-3 top-8 rounded-full bg-Flamingo-Pink ease-[cubic-bezier(0,1,1,0)] animate-ellipsis-4 left-14"></div>
 		</div>
 	)
 }
