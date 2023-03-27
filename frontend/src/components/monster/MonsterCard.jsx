49c2ad366b41af5ad3d19b482a9a56cb07c05b70
phat@minim1 <phat.tranducvn@gmail.com>
2023-03-27 11:46:22 -0700

    Change exp bar background color

--- frontend/src/components/monster/MonsterCard.jsx
+++ frontend/src/components/monster/MonsterCard.jsx
@@ -94,7 +94,7 @@ export default function MonsterCard({
 					<div className="flex flex-col justify-center w-full pt-1">
 						<ProgressBar
 							percentage={Math.floor(exp / level_up_exp)}
-							bgColorClass="bg-white"
+							bgColorClass="bg-Light-Gray"
 							currentBgColorClass="bg-Forest-Green"
 						/>
 						<div className="flex flex-row justify-between items-center mt-1">
