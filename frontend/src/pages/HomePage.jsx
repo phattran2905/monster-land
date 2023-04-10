b284d511a9694c24ffb708456bee60e63fcec1a7
phat@minim1 <phat.tranducvn@gmail.com>
2023-04-09 23:38:45 -0700

    Add Loading when fetching top monsters

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { GiBroadsword, GiLightningShield } from "react-icons/gi"
 import { TbAwardFilled } from "react-icons/tb"
 import { FaChessRook } from "react-icons/fa"
+import Loading from "../components/Loading"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import MenuBar from "../components/menu/MenuBar"
@@ -29,6 +30,7 @@ export default function HomePage() {
 	const [topLevel, setTopLevel] = useState()
 	const [topAttack, setTopAttack] = useState()
 	const [topDefense, setTopDefense] = useState()
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		// Redirect to login if not logged in
@@ -45,6 +47,7 @@ export default function HomePage() {
 
 		if (trainerData) {
 			dispatch(updateTrainerInfo(trainerData))
+			setIsLoading(false)
 		}
 	}, [isFetching])
 
@@ -55,6 +58,7 @@ export default function HomePage() {
 			setTopLevel(level)
 			setTopAttack(attack)
 			setTopDefense(defense)
+			setIsLoading(false)
 		}
 	}, [topMonsters])
 
@@ -72,7 +76,7 @@ export default function HomePage() {
 			<div className="w-full h-full flex flex-row">
 				<MenuBar handleLogout={handleLogout} />
 
-				{/* Top Monster */}
+				{/* Golden Board */}
 				<div className="w-full px-32 py-12 flex flex-col justify-start items-center">
 					{/* Title */}
 					<div className="flex flex-col justify-center items-center mx-auto w-1/2 p-4 bg-Gold-Sand rounded-lg shadow-md">
@@ -86,140 +90,152 @@ export default function HomePage() {
 					</div>
 
 					<div className="mt-32 w-full flex flex-row justify-between items-center">
-						{/* Top Level */}
-						<div className="flex flex-col w-96">
-							{/* Title */}
-							<div className="bg-Flamingo-Pink shadow-md shadow-Fire-Engine-Red rounded-lg p-4 flex flex-row justify-center items-center mb-10">
-								<FaChessRook
-									size={24}
-									className="text-Gold-Sand"
-								/>
-								<h2 className="text-2xl font-bold text-white mx-2">No.1 Level</h2>
-							</div>
-							{/* Image */}
-							<div className="w-full h-[28rem] pb-16 mb-8 rounded-lg shadow-md">
-								<img
-									className="w-full h-full object-scale-down"
-									src={`img/monsters/${topLevel?.img_name}`}
-								/>
-							</div>
-							{/* Details */}
-							<div className="w-full rounded-lg shadow-md shadow-Royal-Blue">
-								{/* Name */}
-								<div className="p-4 bg-Indigo-Blue w-full">
-									<p className="font-bold text-white text-center text-xl">
-										{topLevel?.name}
-									</p>
-								</div>
-								{/* Attribute */}
-								<div className="p-8 flex flex-row justify-around items-center">
-									<div className="flex flex-row items-center">
+						{isLoading ? (
+							<Loading />
+						) : (
+							<>
+								{/* Top Level */}
+								<div className="flex flex-col w-96">
+									{/* Title */}
+									<div className="bg-Flamingo-Pink shadow-md shadow-Fire-Engine-Red rounded-lg p-4 flex flex-row justify-center items-center mb-10">
 										<FaChessRook
-											size={22}
-											className="text-Flamingo-Pink"
+											size={24}
+											className="text-Gold-Sand"
 										/>
-										<span className="text-Indigo-Blue font-bold mx-2 text-lg">
-											Level
-										</span>
+										<h2 className="text-2xl font-bold text-white mx-2">
+											No.1 Level
+										</h2>
 									</div>
-									<div className="bg-Midnight-Gray rounded-lg p-2">
-										<span className="font-bold text-white">
-											{topLevel?.level}
-										</span>
+									{/* Image */}
+									<div className="w-full h-[28rem] pb-16 mb-8 rounded-lg shadow-md">
+										<img
+											className="w-full h-full object-scale-down"
+											src={`img/monsters/${topLevel?.img_name}`}
+										/>
+									</div>
+									{/* Details */}
+									<div className="w-full rounded-lg shadow-md shadow-Royal-Blue">
+										{/* Name */}
+										<div className="p-4 bg-Indigo-Blue w-full">
+											<p className="font-bold text-white text-center text-xl">
+												{topLevel?.name}
+											</p>
+										</div>
+										{/* Attribute */}
+										<div className="p-8 flex flex-row justify-around items-center">
+											<div className="flex flex-row items-center">
+												<FaChessRook
+													size={22}
+													className="text-Flamingo-Pink"
+												/>
+												<span className="text-Indigo-Blue font-bold mx-2 text-lg">
+													Level
+												</span>
+											</div>
+											<div className="bg-Midnight-Gray rounded-lg p-2">
+												<span className="font-bold text-white">
+													{topLevel?.level}
+												</span>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
 
-						{/* Top Attack */}
-						<div className="flex flex-col w-96">
-							{/* Title */}
-							<div className="bg-Flamingo-Pink shadow-md shadow-Fire-Engine-Red rounded-lg p-4 flex flex-row justify-center items-center mb-10">
-								<GiBroadsword
-									size={24}
-									className="text-Gold-Sand"
-								/>
-								<h2 className="text-2xl font-bold text-white mx-2">No.1 Attack</h2>
-							</div>
-							{/* Image */}
-							<div className="w-full h-[28rem] pb-16 mb-8 rounded-lg shadow-md">
-								<img
-									className="w-full h-full object-scale-down"
-									src={`img/monsters/${topAttack?.img_name}`}
-								/>
-							</div>
-							{/* Details */}
-							<div className="w-full rounded-lg shadow-md shadow-Royal-Blue">
-								{/* Name */}
-								<div className="p-4 bg-Indigo-Blue w-full">
-									<p className="font-bold text-white text-center text-xl">
-										{topAttack?.name}
-									</p>
-								</div>
-								{/* Attribute */}
-								<div className="p-8 flex flex-row justify-around items-center">
-									<div className="flex flex-row items-center">
+								{/* Top Attack */}
+								<div className="flex flex-col w-96">
+									{/* Title */}
+									<div className="bg-Flamingo-Pink shadow-md shadow-Fire-Engine-Red rounded-lg p-4 flex flex-row justify-center items-center mb-10">
 										<GiBroadsword
-											size={22}
-											className="text-Flamingo-Pink"
+											size={24}
+											className="text-Gold-Sand"
 										/>
-										<span className="text-Indigo-Blue font-bold mx-2 text-lg">
-											Attack
-										</span>
+										<h2 className="text-2xl font-bold text-white mx-2">
+											No.1 Attack
+										</h2>
 									</div>
-									<div className="bg-Midnight-Gray rounded-lg p-2">
-										<span className="font-bold text-white">
-											{topAttack?.attack}
-										</span>
+									{/* Image */}
+									<div className="w-full h-[28rem] pb-16 mb-8 rounded-lg shadow-md">
+										<img
+											className="w-full h-full object-scale-down"
+											src={`img/monsters/${topAttack?.img_name}`}
+										/>
+									</div>
+									{/* Details */}
+									<div className="w-full rounded-lg shadow-md shadow-Royal-Blue">
+										{/* Name */}
+										<div className="p-4 bg-Indigo-Blue w-full">
+											<p className="font-bold text-white text-center text-xl">
+												{topAttack?.name}
+											</p>
+										</div>
+										{/* Attribute */}
+										<div className="p-8 flex flex-row justify-around items-center">
+											<div className="flex flex-row items-center">
+												<GiBroadsword
+													size={22}
+													className="text-Flamingo-Pink"
+												/>
+												<span className="text-Indigo-Blue font-bold mx-2 text-lg">
+													Attack
+												</span>
+											</div>
+											<div className="bg-Midnight-Gray rounded-lg p-2">
+												<span className="font-bold text-white">
+													{topAttack?.attack}
+												</span>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
 
-						{/* Top Defense */}
-						<div className="flex flex-col w-96">
-							{/* Title */}
-							<div className="bg-Flamingo-Pink shadow-md shadow-Fire-Engine-Red rounded-lg p-4 flex flex-row justify-center items-center mb-10">
-								<GiLightningShield
-									size={24}
-									className="text-Gold-Sand"
-								/>
-								<h2 className="text-2xl font-bold text-white mx-2">No.1 Level</h2>
-							</div>
-							{/* Image */}
-							<div className="w-full h-[28rem] pb-16 mb-8 rounded-lg shadow-md">
-								<img
-									className="w-full h-full object-scale-down"
-									src={`img/monsters/${topDefense?.img_name}`}
-								/>
-							</div>
-							{/* Details */}
-							<div className="w-full rounded-lg shadow-md shadow-Royal-Blue">
-								{/* Name */}
-								<div className="p-4 bg-Indigo-Blue w-full">
-									<p className="font-bold text-white text-center text-xl">
-										{topDefense?.name}
-									</p>
-								</div>
-								{/* Attribute */}
-								<div className="p-8 flex flex-row justify-around items-center">
-									<div className="flex flex-row items-center">
+								{/* Top Defense */}
+								<div className="flex flex-col w-96">
+									{/* Title */}
+									<div className="bg-Flamingo-Pink shadow-md shadow-Fire-Engine-Red rounded-lg p-4 flex flex-row justify-center items-center mb-10">
 										<GiLightningShield
-											size={22}
-											className="text-Flamingo-Pink"
+											size={24}
+											className="text-Gold-Sand"
 										/>
-										<span className="text-Indigo-Blue font-bold mx-2 text-lg">
-											Defense
-										</span>
+										<h2 className="text-2xl font-bold text-white mx-2">
+											No.1 Level
+										</h2>
 									</div>
-									<div className="bg-Midnight-Gray rounded-lg p-2">
-										<span className="font-bold text-white">
-											{topDefense?.defense}
-										</span>
+									{/* Image */}
+									<div className="w-full h-[28rem] pb-16 mb-8 rounded-lg shadow-md">
+										<img
+											className="w-full h-full object-scale-down"
+											src={`img/monsters/${topDefense?.img_name}`}
+										/>
+									</div>
+									{/* Details */}
+									<div className="w-full rounded-lg shadow-md shadow-Royal-Blue">
+										{/* Name */}
+										<div className="p-4 bg-Indigo-Blue w-full">
+											<p className="font-bold text-white text-center text-xl">
+												{topDefense?.name}
+											</p>
+										</div>
+										{/* Attribute */}
+										<div className="p-8 flex flex-row justify-around items-center">
+											<div className="flex flex-row items-center">
+												<GiLightningShield
+													size={22}
+													className="text-Flamingo-Pink"
+												/>
+												<span className="text-Indigo-Blue font-bold mx-2 text-lg">
+													Defense
+												</span>
+											</div>
+											<div className="bg-Midnight-Gray rounded-lg p-2">
+												<span className="font-bold text-white">
+													{topDefense?.defense}
+												</span>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
+							</>
+						)}
 					</div>
 				</div>
 			</div>
