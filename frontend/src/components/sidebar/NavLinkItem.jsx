5b1d029ef74464833f2411b6cb64ebcf041ca5a2
phat@minim1 <phat.tranducvn@gmail.com>
2024-02-25 17:31:11 -0800

    Add ImageIcon

--- frontend/src/components/sidebar/NavLinkItem.jsx
+++ frontend/src/components/sidebar/NavLinkItem.jsx
@@ -1,40 +1,42 @@
-import { NavLink } from "react-router-dom"
-import { useState } from "react"
-import ImageIcon from "../ImageIcon"
+import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
+import ImageIcon from '@components/ImageIcon'
 
 const activeClassName =
-	"w-full h-20 p-2 flex flex-row justify-center items-center bg-Flamingo-Pink transition-colors duration-500"
+	'w-full h-20 p-2 flex flex-row justify-center items-center bg-Flamingo-Pink transition-colors duration-500'
 
 const inactiveClassName =
-	"w-full h-20 p-2 flex flex-row justify-center items-center bg-Royal-Blue hover:bg-Flamingo-Pink transition-colors duration-500"
+	'w-full h-20 p-2 flex flex-row justify-center items-center bg-Royal-Blue hover:bg-Flamingo-Pink transition-colors duration-500'
 
 export default function NavLinkItem({ path, handler }) {
 	const [iconName] = useState(() => {
 		switch (path) {
-			case "/backpack":
-				return "backpack"
-			case "/trainer":
-				return "user"
-			case "/challenges":
-				return "challenges"
-			case "/collection":
-				return "monster-collection"
-			case "/team":
-				return "team"
-			case "/incubation":
-				return "incubation"
-			case "/logout":
-				return "power-off"
+			case '/backpack':
+				return 'backpack'
+			case '/trainer':
+				return 'user'
+			case '/challenges':
+				return 'challenges'
+			case '/collection':
+				return 'monster-collection'
+			case '/team':
+				return 'team'
+			case '/incubation':
+				return 'incubation'
+			case '/logout':
+				return 'power-off'
 		}
 	})
 
 	return (
 		<NavLink
-            onClick={handler}
+			onClick={handler}
 			to={path}
-			className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}
+			className={({ isActive }) =>
+				isActive ? activeClassName : inactiveClassName
+			}
 		>
-			<ImageIcon name={iconName} />
+			<ImageIcon type={iconName} />
 		</NavLink>
 	)
 }
