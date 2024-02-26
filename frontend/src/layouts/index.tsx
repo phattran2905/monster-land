a20b110de226df83a189c432a7c4f4ad03c8bbc1
phat@minim1 <phat.tranducvn@gmail.com>
2024-02-25 17:03:01 -0800

    Improve Layout component

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,21 +1,3 @@
-import React from 'react'
-import Page from './Page'
-import AuthPage from '@layouts/AuthPage'
-import ErrorPage from '@layouts/ErrorPage'
+import Layout from './Layout'
 
-interface LayoutProps {
-	type: 'authPage' | 'errorPage' | 'page'
-	children: React.ReactNode
-}
-
-const Layout = ({ type, ...props }: LayoutProps) => {
-	switch (type) {
-		case 'authPage':
-			return <AuthPage {...props} />
-		case 'errorPage':
-			return <ErrorPage {...props} />
-		default:
-			return <Page {...props} />
-	}
-}
 export default Layout
