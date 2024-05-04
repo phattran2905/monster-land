176fab8c7356a7fdb302ac63321df7a053f5443f
phattran2905 <phat@gearboxbuilt.com>
2024-05-04 15:54:51 -0700

    Add types for profile and account

---
+++ apps/web/types/auth.ts
@@ -0,0 +1,6 @@
+export interface Account {
+	created_at: string
+	email: string
+	id: string
+	phone: string
+}
---
+++ apps/web/types/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+	avatar: string
+	created_at: string
+	email?: string
+	exp: number
+	level_up_exp: number
+	stamina: number
+	uid: string
+	username: string
+}
